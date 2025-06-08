902c6bb8d108d4f1b32da90d38acecc994427ed0
Lucio <lunicolini@uade.edu.ar>
2025-06-08 20:10:28 -0300

    Estructura inicial del proyecto

---
+++ src/test/java/vidasana/tpo/TpoApplicationTests.java
@@ -0,0 +1,13 @@
+package vidasana.tpo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TpoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
